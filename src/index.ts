e667739846172859e059e072e7346121038da819
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-10-15 15:20:49 +0700

    port

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,7 @@ import app from "./app"
 import { port } from "./configs/app.config"
 import connectDB from "./configs/db.config"
 
-app.listen(3000, "0.0.0.0", () => {
+app.listen(port, () => {
   connectDB()
-<<<<<<< HEAD
   console.log(`Server listen on http://localhost:${port}/`)
-=======
-  console.log(`Server is listening on http://localhost:${3000}/`)
->>>>>>> develop
 })
